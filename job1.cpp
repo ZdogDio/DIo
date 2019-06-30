f46fdf1201191a9d78af325ed44769b83030d106
ZdogDio <52146637+ZdogDio@users.noreply.github.com>
2019-06-30 22:41:55 +0700

    Add files via upload

---
+++ job1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout <<"Student Code : 62-060216-1014-5 \n";
+	cout <<"Student name : Prueksatol Rujiwan \n";
+	cout <<"Division     :Infotmation Technology\n";
+	cout <<"Year         :1\n";
+	cout <<"Room         :A\n";
+	cout <<"Sax          :Male\n";
+	cout <<"Age          :18\n";
+
+	cout <<"   #\n";
+	cout <<"  # #\n";
+	cout <<" #   #\n";
+	cout <<"#     #\n";
+	cout <<"#######\n";
+	cout <<"#     #\n";
+}
